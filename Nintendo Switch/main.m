de68b1060c815f26b42e053b69fa05e885a4b40e
Eduardo Vital Alencar Cunha <vital@mac308.local>
2017-03-10 16:05:59 -0300

    Initial Commit

---
+++ Nintendo Switch/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Nintendo Switch
+//
+//  Created by Eduardo Vital Alencar Cunha on 10/03/17.
+//  Copyright © 2017 BEPiD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
